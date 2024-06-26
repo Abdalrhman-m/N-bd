26d5ad0d9029f1a601edcff709e82b7ff5d44e4a
Abdalrhman-m <sgud776@gmail.com>
2024-06-27 00:58:07 +0300

    Update effect.js

--- assets/effect.js
+++ assets/effect.js
@@ -173,9 +173,9 @@ $('document').ready(function(){
 		var i;
 
 		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(1200).promise().done(function(){
+			$("p:nth-child("+i+")").fadeOut('slow').delay(500).promise().done(function(){
 			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1700);
+			$("p:nth-child("+i+")").fadeIn('slow').delay(2700);
 			if(i==50){
 				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
